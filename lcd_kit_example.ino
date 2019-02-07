8969ef463e0a208eaa7edafe36140fca6871c86b
MaSanto Raffiki <Davidpaez1006@gmail.com>
2019-02-06 19:56:55 -0700

    Update lcd_kit_example.ino
    Hopefully this makes a little sense. I just attempted to condense the amount of function listed and replace them within variable format within on bigger functions. I have not tested this code on the screen... Purely Theory.

--- lcd_kit_example.ino
+++ lcd_kit_example.ino
@@ -1,18 +1,3 @@
-/*
- * Here's a little example code for the Sparkfun Serial LCD Kit 
- * that includes a 16 x 2 liquid crystal display 
- * 
- * Hardware setup (used in this case):
- * Plug in power and ground respectively, then yellow wire (RX), to D3 on Arduino UNO
- * 
- * More commands and special commands can be found on github at: 
- * https://github.com/jimblom/Serial-LCD-Kit/wiki/Serial-Enabled-LCD-Kit-Datasheet
- * 
- * Code assembled and collected by Ty 
- * 
- */
- 
-//_-_-_-_-_-_-_-_-_ Initial Setup _-_-_-_-_-_-_-_-_-_-_
 
 #include <SoftwareSerial.h>
 
@@ -23,52 +8,33 @@ void setup()
   lcd.begin(9600);             // Start the LCD at 9600 baud
   clearDisplay();              // Clear the display
   
-  setLCDCursor(2);             
+  printState('still',2,200);            
   lcd.print("Hello, world!");  // Statement that prints to 1st row, 3rd column
-  setLCDCursor(17);            
+  printState('still',17,200);            
   lcd.print("Begin Setup..."); // Statement that prints SIMULTANEOUSLY to 2nd row, 2nd column
   delay(2000);
   
   for (int i=0; i<3; i++)      // Flashes the backlight while above statements display on startup:
   {
-    setBacklight(0);           // Minimum Brightness of 0
-    delay(250);
-    setBacklight(255);         // Maximum Brightness of 255
-    delay(250);               
+    printState('still',0,0);
+    delay(300);
+    printState('still',0,255);
+    delay(300);          
   }
 }
-
-//_-_-_-_-_-_-_-_-_-_-_   Loop  _-_-_-_-_-_-_-_-_-_-_-_
 
 void loop()
  {
     clearDisplay();            // Clear the display
     delay(500);
-    setLCDCursor(4);           // set cursor to 1st row, 5th column
+    printState('still',4,155);           // set cursor to 1st row, 5th column
     lcd.print("LCD TEST");
     delay(1000);
-    setLCDCursor(17);          // set cursor to 2nd row, 3rd column
+    printState('still',17,200);          // set cursor to 2nd row, 3rd column
     lcd.print("Great Success!");
     delay(3000);
-
+    screenState('off');
   }
-
-// _-_-_-_-_-_-_ Set ups for Byte Definitions _-_-_-_-_-_-_
-//        (uncomment, copy, paste, and use as needed)
-
-void setBacklight(byte brightness) // adjusts backlight brightness from 0-255
-{
-  lcd.write(0x80);             // send the backlight command
-  lcd.write(brightness);       // send the brightness value
-}
-/*
-void setBaudRate (byte baud_rate)  // Sets the baud rate 
-{
-  lcd.write(0x81);             // send the baud rate command
-  lcd.write(baud_rate);        // send the baud value from 0 - 10 to correspond to 300 - 115200
-}
-*/
-//____________THIS SECTION IS SPECIAL COMMANDS ONLY______________
 
 void clearDisplay()            // Clears out the display, MUST USE for this kit!
 {
@@ -76,35 +42,31 @@ void clearDisplay()            // Clears out the display, MUST USE for this kit!
   lcd.write(0x01);             // send the clear screen command
 }
 
-void setLCDCursor(byte cursor_position)  
+
+void printState(char dire, byte pos, byte brightness)       // Scrolls the text to the right, disappears on same row
 {
-  lcd.write(0xFE);  
-  lcd.write(0x80);  
-  lcd.write(cursor_position);  // Enter value from 0 - 31 for cursor postion 
+  lcd.write(0x80);             // send the backlight command
+  lcd.write(brightness);
+  
+  if(dire == 'right'){
+    lcd.write(0xFE);  
+    lcd.write(0x1C);}
+  else if(dire == 'left'){
+    lcd.write(0xFE);  
+    lcd.write(0x18);}
+  else if (dire == 'still'){
+    lcd.write(0xFE);  
+    lcd.write(0x80);  
+    lcd.write(pos);}
 }
 
-/*
-void scrollCursorRight()       // Scrolls the text to the right, disappears on same row
-{
-  lcd.write(0xFE);  
-  lcd.write(0x1C);  
-}
-
-void scrollCursorLeft()        // Scrolls the text to the left
-{
-  lcd.write(0xFE);  
-  lcd.write(0x18);  
-}
-
-void turnScreenOff()           // Turns the display OFF 
-{
+void screenState(char state){
+  if(state == 'off'){
   lcd.write(0xFE);  
   lcd.write(0x08);  
 }
-
-void turnScreenOn()            // Turns the display ON
-{
+else if(state == 'on'){
   lcd.write(0xFE);  
   lcd.write(0x18);  
 }
-*/
+}
